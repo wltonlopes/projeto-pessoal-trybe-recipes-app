0d70bd32a5293c280a42677331557bba14d2d7f7
Matheus Souza <ms.conrado25@hotmail.com>
2021-11-05 17:03:27 -0300

    reisitos 2 ao 8 - Tela de login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const CARACTER_MIN = 6;
+  const VALIDATE_EMAIL = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    localStorage.setItem('mealsToken', JSON.stringify(1));
+    localStorage.setItem('cocktailsToken', JSON.stringify(1));
+    localStorage.setItem('user', JSON.stringify({ email }));
+    history.push('/comidas');
+  };
+
+  return (
+    <div>
+      <form onSubmit={ handleClick }>
+        <input
+          type="email"
+          data-testid="email-input"
+          onChange={ ({ target }) => setEmail(target.value) }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          onChange={ ({ target }) => setPassword(target.value) }
+        />
+        <button
+          type="submit"
+          data-testid="login-submit-btn"
+          disabled={
+            password.length <= CARACTER_MIN
+              || email.search(VALIDATE_EMAIL) !== 0
+          }
+        >
+          Enviar
+        </button>
+      </form>
+    </div>
+  );
+}
