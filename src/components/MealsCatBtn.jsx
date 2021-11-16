ec0fc22091417b5e5c713e12f0eb31688a5f3475
Lucas G Silveira <lucgs88@gmail.com>
2021-11-16 17:21:15 -0300

    adicionado comments para futura improvement

--- src/components/MealsCatBtn.jsx
+++ src/components/MealsCatBtn.jsx
@@ -5,29 +5,34 @@ import { SearchFood } from '../services/SearchFood';
 const MAX_LENGTH = 4;
 
 export default function MealsCatBtn() {
-  const { mealsCat, setRevenues } = useContext(RevenuesContex);
+  const { mealsCat, setRevenues, revenues } = useContext(RevenuesContex);
   const [toggle, setToggle] = useState('');
+  const [unToggle, setUnToggle] = useState([]);
+  // os comments abaixo são para evitar que o usuário aperte o botão mais de uma vez
+  // e chame de novo a api com o conteúdo com que já foi chamado anteriormente.
+  // const [previousFood, setPreviousFood] = useState([]);
 
   const eventHandler = async ({ target }) => {
     const { value } = target;
     setToggle(value);
 
     if (toggle === value || value === 'All') {
-      const response = await SearchFood(
-        'https://www.themealdb.com/api/json/v1/1/search.php?s=',
-      );
-      setRevenues(response.meals);
+      setRevenues(unToggle);
       setToggle('');
     } else {
+      // if (value === response.meals) {
+      //   setRevenues(previousFood);
+      // }
       const response = await SearchFood(
         `https://www.themealdb.com/api/json/v1/1/filter.php?c=${value}`,
       );
+      // setPreviousFood(response.meals);
+      setUnToggle(revenues);
       setRevenues(response.meals);
     }
   };
 
   const meals = mealsCat.filter((meal, index) => index <= MAX_LENGTH);
-
   return (
     <form>
       { meals.map(({ strCategory }, index) => (
