77c2050854edf88cf03305b738fac1b27a8ea56a
Welton Lopes <weltonlopesmetal@gmail.com>
2021-11-17 15:08:29 -0300

    Requisitos 83 e 84

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const [user, setUser] = useState('');
+  useEffect(() => {
+    const { email } = JSON.parse(localStorage.getItem('user'));
+    setUser(email);
+  }, []);
+
   return (
     <div>
       <Header title="Perfil" />
       <h1>Profile</h1>
-      <p data-testid="profile-email">Email</p>
+      <p data-testid="profile-email">{ user }</p>
       <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
       <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
       <button type="button" data-testid="profile-logout-btn">Sair</button>
