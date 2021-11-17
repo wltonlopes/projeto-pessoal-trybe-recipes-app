2299b31ac23020ba801ca1a02f846808ddda4020
Matheus Souza <ms.conrado25@hotmail.com>
2021-11-16 21:20:04 -0300

    tela principal feita pelo Lucas por conta dos conflitos eu tive que refazer ela em outra branch

--- src/pages/drinks/ExploreDrinks.jsx
+++ src/pages/drinks/ExploreDrinks.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import { SwitchDrink } from '../../services/SearchDrink';
 
 function ExploreDrinks() {
+  const history = useHistory();
+
+  const handleClickRandom = async () => {
+    const result = await SwitchDrink('random');
+    const id = result.drinks[0].idDrink;
+    history.push(`/bebidas/${id}`);
+  };
   return (
     <div>
       <Header title="Explorar Bebidas" />
-      <h1>ExploreDrinks</h1>
+      <div data-testid="explore-drinks">
+        <h1>Explore Drinks</h1>
+        <Link to="/explorar/bebidas/ingredientes">
+          <button
+            type="submit"
+            data-testid="explore-by-ingredient"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+
+        <Link to="/explorar/bebidas">
+          <button
+            type="submit"
+            data-testid="explore-surprise"
+            onClick={ handleClickRandom }
+          >
+            Me Surpreenda!
+          </button>
+        </Link>
+      </div>
       <Footer data-testid="drinks-bottom-btn" />
     </div>
   );
--- src/services/SearchDrink.js
+++ src/services/SearchDrink.js
@@ -1,6 +1,7 @@
 const NAME = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 const FIRST_LETTER = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=';
 const INGREDIENT = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
+const RANDOM = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
 
 export const SearchDrink = async (api) => {
   const result = await fetch(api);
@@ -16,6 +17,8 @@ export const SwitchDrink = (radios, value) => {
     return SearchDrink(`${NAME}${value.split(' ').splice('_')}`);
   case 'first-letter':
     return SearchDrink(`${FIRST_LETTER}${value}`);
+  case 'random':
+    return SearchDrink(`${RANDOM}`);
   default:
     break;
   }
