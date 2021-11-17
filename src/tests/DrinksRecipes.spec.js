c597775bada3752f36934254b30ee3dd6c93d6e5
Gabriel Franklin <franklingabriel158@gmail.com>
2021-11-17 14:12:10 -0300

    Teste DrinksRecipes em 94%

---
+++ src/tests/DrinksRecipes.spec.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { mockDrinkRecipe, mockDrinkRecomendations } from './utils/mock';
+import App from '../App';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('Receita de Bebidas', () => {
+  const IMAGE_DRINK = 'recipe-photo';
+  const TITLE_DRINK = 'recipe-title';
+  const FAVORITE_BUTTON = 'favorite-btn';
+  const SHARE_BUTTON = 'share-btn';
+  const CONTINUE_RECIPE = 'start-recipe-btn';
+  const pathWay = '/bebidas/17222';
+  const INGREDIENTS_LENGTH = 4;
+  const RECOMENDATIONS_LENGTH = 6;
+
+  beforeEach(async () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(pathWay);
+    await waitForElementToBeRemoved(() => screen.getByText(/Carregando.../i),
+      { timeout: 3000 });
+  });
+
+  it('Verfica se "Carregando..." aparece antes de mostar a receita', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(pathWay);
+
+    expect(screen.getByText(/Carregando.../i)).toBeInTheDocument();
+  });
+  it('Verifica se a imagem e o título da bebida aparece na tela', () => {
+    const imageDrink = screen.getByTestId(IMAGE_DRINK);
+    const titleDrink = screen.getByTestId(TITLE_DRINK);
+    expect(imageDrink).toBeInTheDocument();
+    expect(titleDrink).toBeInTheDocument();
+  });
+  it('Verifica se os botões "Compartilhar" e "Favoritar" estão na tela', () => {
+    const favoriteButton = screen.getByTestId(FAVORITE_BUTTON);
+    const shareButton = screen.getByTestId(SHARE_BUTTON);
+    expect(favoriteButton).toBeInTheDocument();
+    expect(shareButton).toBeInTheDocument();
+  });
+  it('Verifica se a categoria da Receita esta na tela', async () => {
+    expect(screen.getByTestId('recipe-category')).toBeInTheDocument();
+  });
+  it('Verifica se renderiza os ingredientes na tela', () => {
+    const ingredients = mockDrinkRecipe.map((drink, index) => {
+      const show = screen.getByTestId(`${index}-ingredient-name-and-measure`);
+      return show;
+    });
+    expect(ingredients).toHaveLength(INGREDIENTS_LENGTH);
+  });
+  it('Verifica se as instruções estão na tela', () => {
+    expect(screen.getByTestId('instructions')).toBeInTheDocument();
+  });
+  it('Verifica se as recomendações estão na tela', () => {
+    const recomendations = mockDrinkRecomendations.map((recomend, index) => {
+      const recomende = screen.getByTestId(`${index}-recomendation-title`);
+      return recomende;
+    });
+    expect(recomendations).toHaveLength(RECOMENDATIONS_LENGTH);
+  });
+  it(('Verifica se o botão "Continuar Receita" esta na tela'), () => {
+    expect(screen.getByTestId(CONTINUE_RECIPE)).toBeInTheDocument();
+  });
+  it(('Verifica clique no botão'), () => {
+    const continueButton = screen.getByTestId(CONTINUE_RECIPE);
+    expect(continueButton).toBeInTheDocument();
+    userEvent.click(continueButton);
+    const finalButton = screen.getByTestId('finish-recipe-btn');
+    expect(finalButton).toBeInTheDocument();
+  });
+});
---
+++ src/tests/utils/mock.js
@@ -0,0 +1,34 @@
+export const mockFood = [{
+  idMeal: '52768',
+  strMeal: 'Apple Frangipan Tart',
+  strMealThumb: 'https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg',
+}, {
+  idMeal: '52893',
+  strMeal: 'Apple & Blackberry Crumble',
+  strMealThumb: 'https://www.themealdb.com/images/media/meals/xvsurr1511719182.jpg',
+}];
+
+export const mockDrink = [{
+  idDrink: '17222',
+  strDrink: 'A1',
+  strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg',
+}, {
+  idDrink: '13501',
+  strDrink: 'ABC',
+  strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/tqpvqp1472668328.jpg',
+},
+];
+export const mockDrinkRecipe = [
+  'Gin - 1 3/4 shot',
+  'Grand Marnier - 1 Shot',
+  'Lemon Juice - 1/4 Shot',
+  'Grenadine - 1/8 Shot',
+];
+export const mockDrinkRecomendations = [
+  'Corba',
+  'Burek',
+  'Kumpir',
+  'Tamiya',
+  'Dal fry',
+  'Poutine',
+];
