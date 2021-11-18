e2b71b53aa7bc4869246f0cba4c1f63aeebaeaba
AlineCarolina <eualine2017@gmail.com>
2021-11-18 18:38:50 -0300

    Requisito 75 e 76

--- src/pages/drinks/ExploreDrinksIngre.jsx
+++ src/pages/drinks/ExploreDrinksIngre.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import { SearchDrink } from '../../services/SearchDrink';
 
 function ExploreDrinksIngre() {
+  const MAX_LENGTH = 12;
+  const [data, setData] = useState([]);
+
+  const fetchIngredients = async () => {
+    const resultApi = await SearchDrink('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list');
+    const recipes = resultApi.drinks.slice(0, MAX_LENGTH);
+    setData(recipes);
+  };
+
+  useEffect(() => { fetchIngredients(); }, []);
+
   return (
-    <div>
+    <section>
       <Header title="Explorar Ingredientes" />
-      <h1>ExploreDrinksIngre</h1>
+      <main>
+        { data.map((ingre, index) => (
+          <section data-testid={ `${index}-ingredient-card` } key={ index }>
+            <h1
+              data-testid={ `${index}-card-name` }
+            >
+              { ingre.strIngredient || ingre.strIngredient1 }
+            </h1>
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ `https://www.thecocktaildb.com/images/ingredients/${ingre.strIngredient || ingre.strIngredient1}-Small.png ` }
+              alt={ ingre.strIngredient || ingre.strIngredient1 }
+            />
+          </section>
+        )) }
+      </main>
       <Footer />
-    </div>
+    </section>
   );
 }
 
--- src/pages/food/ExploreFoodIngre.jsx
+++ src/pages/food/ExploreFoodIngre.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import { SearchDrink } from '../../services/SearchDrink';
 
-function ExploreFoodIngre() {
+export default function ExploreFoodIngredients() {
+  const MAX_LENGTH = 12;
+  const [data, setData] = useState([]);
+
+  const fetchIngredients = async () => {
+    const resultApi = await SearchDrink('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+    const recipes = resultApi.meals.slice(0, MAX_LENGTH);
+    setData(recipes);
+  };
+
+  useEffect(() => { fetchIngredients(); }, []);
+
   return (
-    <div>
+    <section>
       <Header title="Explorar Ingredientes" />
-      <h1>ExploreFoodIngre</h1>
+      <main>
+        { data.map((ingre, index) => (
+          <section data-testid={ `${index}-ingredient-card` } key={ index }>
+            <h1
+              data-testid={ `${index}-card-name` }
+            >
+              { ingre.strIngredient || ingre.strIngredient1 }
+            </h1>
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ `https://www.themealdb.com/images/ingredients/${ingre.strIngredient || ingre.strIngredient1}-Small.png ` }
+              alt={ ingre.strIngredient || ingre.strIngredient1 }
+            />
+          </section>
+        )) }
+      </main>
       <Footer />
-    </div>
+    </section>
   );
 }
-
-export default ExploreFoodIngre;
